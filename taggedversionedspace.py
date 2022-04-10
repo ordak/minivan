def5f8c49295ba5c4474b95a01004ec9fb6a4e33
preston <wpgalle3@gmail.com>
2022-04-09 23:15:49 -0700

    adding type hints to new classes

--- taggedversionedspace.py
+++ taggedversionedspace.py
@@ -1,28 +1,31 @@
-from taggedversionstate import TaggedVersionState
-from toolz import dicttoolz as dtz
+from typing import List, Dict, Any
 import functools
+
+from toolz import dicttoolz as dtz
+
+from taggedversionedstatemixin import TaggedVersionedStateMixin
 
 class TaggedVersionedSpace:
     def __init__(self, *args):
-        self._constituents__tag = dict()
-        for constituent in *args:
+        self._constituents__tag: Dict[str, TaggedVerionedStateMixin] = dict()
+        for constituent in args:
             self.add(constituent.get_tag(), constituent)
 
-    def add(self, tag, constituent: TaggedVersionState):
+    def add(self, tag: str, constituent: TaggedVersionedStateMixin):
         self._constituents__tag[tag] = constituent
 
     def get_latest_versions(self):
         return dtz.valmap(
-            lambda constituent => constituent.get_latest_version(),
+                lambda constituent: constituent.get_latest_version(),
             self._constituents__tag)
 
-    def get_changed_states_only(self, old_version__tag):
+    def get_changed_states_only(self, old_version__tag: Dict[str, int]):
         rd = dict()
         for tag, constituent in self_constituents__tag.items():
             if tag in old_version__tag:
                 old_version = old_version__tag[tag]
-                cur_version = constituent.get_latest_version():
-                include = old_version != cur_version:
+                cur_version = constituent.get_latest_version()
+                include = old_version != cur_version
             else:
                 cur_version = constituent.get_latest_version()
                 include = True
--- taggedversionedstatemixin.py
+++ taggedversionedstatemixin.py
@@ -1,16 +1,20 @@
+from typing import List, Dict, Any
 
 class TaggedVersionedStateMixin:
-    def __init__(self, tag, initial_state=None):
+    def __init__(self, tag: str, initial_state=None):
         # class could operate fine without knowing its tag
         # this is for convenient reference by upper layer (TaggedVersionedSpace)
-        self._tag = tag
+        self._tag: str = tag
         self._state = initial_state
-        self._version = 0
+        self._version: int = 0
 
-    def get_latest_version(self):
+    def get_tag(self) -> str:
+        return self._tag
+
+    def get_latest_version(self) -> int:
         return self._version
 
-    def get_version_and_value(self, version):
+    def get_version_and_value(self, version: int) -> Dict[str, Any]:
         return {'version': self._version, 'value': self._state}
 
     def updated_versioned_state(self, new_state):
